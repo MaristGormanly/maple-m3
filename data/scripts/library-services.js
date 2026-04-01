4f4495985016e2b1b40f309f300c2656a5db01f1
sufia1117 <skmarch34@gmail.com>
2026-04-01 12:30:29 -0400

    Updated Package files and directory path fixes

--- data/scripts/library-services.js
+++ data/scripts/library-services.js
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 const { Client } = require('pg');
 const { chromium } = require('playwright');
 const { OpenAI } = require('openai'); // Added OpenAI import

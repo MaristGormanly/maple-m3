497916c8f9a70951a687a83b0b47748198808573
sufia1117 <skmarch34@gmail.com>
2026-04-01 12:01:32 -0400

    Updated Repo structure for Scripts
    Run scripts commands accordingly. Cd to data directory and run "npx playwright install" to run the scripts

--- data/scripts/library.js
+++ data/scripts/library.js
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
+require('dotenv').config({ path: path.join(__dirname, '../../server/.env') });
 const { Client } = require('pg');
 const { chromium } = require('playwright'); // Swapping Cheerio out for Playwright
 const { OpenAI } = require('openai');
